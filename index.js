b87c10ac279242c961b102906339c3ea78d75b88
Presbury Wamae <wamaedev@gmail.com>
2025-12-01 16:20:57 +0300

    Added npm modules to see the tests & the variables with appropriate names and used various methods like charAt() & toUpperCase() to get the tests passing

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Presbury Wamae';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+// const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
